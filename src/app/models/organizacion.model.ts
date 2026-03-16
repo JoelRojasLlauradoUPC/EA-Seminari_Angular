075a9721e893d4a50fd29e558cc06ac44824ee0e
JoelRojasLlauradoUPC <joel.rojas@estudiantat.upc.edu>
2026-03-16 23:30:04 +0100

    Comentaris per video

--- src/app/models/organizacion.model.ts
+++ src/app/models/organizacion.model.ts
@@ -1,8 +1,8 @@
 import type { Usuario } from './usuario.model';
 
+//TASCA 1: afegim els usuaris a l'organització (array de tipus Usuario)
 export interface Organizacion {
   _id: string;
   name: string;
   usuarios: Usuario[];
 }
-//TASCA 1: afegim els usuaris a l'organització (array de tipus Usuario)
